e4104a2a66b8aac394f25b65805ff3159a347c33
AYM1607 <u.g.a.mariano@gmail.com>
2020-11-10 21:23:17 -0600

    Added firebase initialization

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD9L_NZwSg7bP0glGZtUZqvCJ_cuMEyQRo",
+  authDomain: "ciudad-de-los-ninos.firebaseapp.com",
+  databaseURL: "https://ciudad-de-los-ninos.firebaseio.com",
+  projectId: "ciudad-de-los-ninos",
+  storageBucket: "ciudad-de-los-ninos.appspot.com",
+  messagingSenderId: "999883307627",
+  appId: "1:999883307627:web:2923b482fdabd29b100f2e",
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
